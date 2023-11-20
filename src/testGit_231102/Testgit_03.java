4e43e6dce2e36d6e0a6541c2deaed38e0d2e8988
pjseok <pjs7850@naver.com>
2023-11-20 12:15:16 +0900

    깃허브 업로드 업데이트 02- Testgit03번 추가

--- src/testGit_231102/Testgit_03.java
+++ src/testGit_231102/Testgit_03.java
@@ -1,11 +1,10 @@
 package testGit_231102;
 
-public class Testgit_02 {
+public class Testgit_03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// 깃허브 업로드용 프로제트 연습파일
-		// 첫번째 수정
+		// 깃허브 업로드 테스트 03번
 	}
 
 }
